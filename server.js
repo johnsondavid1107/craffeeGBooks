26f54da1269d830d8462c05fd1974a8ea9c2ce6f
Ness <johnsondavid1107@gmail.com>
2021-02-06 22:14:45 -0500

    trying to fix heroku deploy

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/search", routes)
 app.use('/save', routes)
 app.use('/find', routes)
 
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
