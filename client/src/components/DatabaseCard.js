2669bf7602cbfd69d49236450b10e97dc0cc24db
Ness <johnsondavid1107@gmail.com>
2021-02-09 12:55:41 -0500

    updates to saved books card

--- client/src/components/DatabaseCard.js
+++ client/src/components/DatabaseCard.js
@@ -6,12 +6,13 @@ function DatabaseCard(props) {
     const { title, author, picture, description, subtitle, pageCount, id } = props
     const styles = {
         buttonMargin: {
-            margin: "5px"
+            marginTop: "10px",
+            marginRight: "10px"
         },
         spaceCard: {
             marginBottom: "10px"
-        }, addPadding: {
-            padding: "10px"
+        }, addMargin: {
+            margin: "10px"
         }
     }
 
@@ -26,19 +27,21 @@ function DatabaseCard(props) {
         <div className="container">
             <div className="card" style={styles.spaceCard}>
                 <div className="row" style={styles.addPadding}>
-                    <div className="col"><strong>{title || "No Title Found.."} </strong> </div>
 
-                    <div className="col-1 d-flex justify-content-end">
+                    <div className="col d-flex justify-content-end">
                         <button className="button is-small is-danger is-outlined" style={styles.buttonMargin} onClick={handleClick} value={id}>Delete</button>
                     </div>
                 </div>
-                <div className="row" style={styles.addPadding}>
+                <div className="row">
+                    <div className="col" style={styles.addMargin}><strong>{title || "No Title Found.."} </strong> </div>
+                </div>
+                <div className="row" style={styles.addMargin}>
                     <div className="col">{subtitle}</div>
                 </div>
-                <div className="row" style={styles.addPadding}>
+                <div className="row" style={styles.addMargin}>
                     <div className="col">{author}</div>
                 </div>
-                <div className="row" style={styles.addPadding}>
+                <div className="row" style={styles.addMargin}>
                     <div className="col-3"><img src={picture || "No Image found..."} alt="book "></img></div>
                     <div className="col-9">{description || "No book description found..."}</div>
                 </div>
